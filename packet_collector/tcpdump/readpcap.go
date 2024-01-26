132421e3213b19930878622575143ede03cedd16
Vincent <sheh.v123@gmail.com>
2024-01-26 08:02:00 +0800

    tcpdump

---
+++ packet_collector/tcpdump/readpcap.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/layers"
+    "github.com/google/gopacket/pcap"
+)
+
+func main() {
+    pcapFile := "mycapture.pcap"
+
+    handle, err := pcap.OpenOffline(pcapFile)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer handle.Close()
+
+    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+    for packet := range packetSource.Packets() {
+        printPacketInfo(packet)
+    }
+}
+
+func printPacketInfo(packet gopacket.Packet) {
+    // Handle Ethernet layer
+    ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+    if ethernetLayer != nil {
+        ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+        fmt.Printf("Source MAC: %s, Destination MAC: %s\n", ethernetPacket.SrcMAC, ethernetPacket.DstMAC)
+    }
+
+    // Handle IPv4 layer
+    ipv4Layer := packet.Layer(layers.LayerTypeIPv4)
+    if ipv4Layer != nil {
+        ipv4Packet, _ := ipv4Layer.(*layers.IPv4)
+        fmt.Printf("IPv4 - Source IP: %s, Destination IP: %s\n", ipv4Packet.SrcIP, ipv4Packet.DstIP)
+    }
+
+    // Handle IPv6 layer
+    ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
+    if ipv6Layer != nil {
+        ipv6Packet, _ := ipv6Layer.(*layers.IPv6)
+        fmt.Printf("IPv6 - Source IP: %s, Destination IP: %s\n", ipv6Packet.SrcIP, ipv6Packet.DstIP)
+    }
+}

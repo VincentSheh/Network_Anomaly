94713d076175730c05b93b68b3d91934b64ad67a
Sheh Vincent <sheh.v123@gmail.com>
2024-01-26 18:20:22 +0700

    pcap reader

--- packet_collector/tcpdump/capture.sh
+++ packet_collector/tcpdump/capture.sh
@@ -3,13 +3,13 @@
 while true; do
     # Run packet capture for 5 minutes
     filename="capture_$(date +%Y%m%d%H%M%S).pcap"
-    tcpdump -i any -w $filename -G 10 -W 1
+    tcpdump -w $filename -G 10 -W 1
 
     # Delete the oldest pcap file
     # Find all pcap files, sort them, and delete all but the two most recent
 
     ls -1tr capture_*.pcap | head -n -2 | xargs -d '\n' rm -f --
-    
+    sleep 1
     # Run Go code to process the two latest pcap files
     go run readpcap.go
 

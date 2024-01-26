0141c7f9b358ec457f6f6b68701b251599064cb0
Sheh Vincent <sheh.v123@gmail.com>
2024-01-26 13:01:36 +0700

    pcap reader

--- packet_collector/main.go
+++ packet_collector/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
+	"path/filepath"
 
 	// "net"
 	"packet_collector/features"
@@ -59,11 +59,13 @@ func getPacketInfo(local_ip string, p *gopacket.Packet) (
 	return ipsrc, ipdst, tcpsrc, tcpdst, direction, netFlow
 }
 
-func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, int64) {
+func processPackets(
+	packetSource *gopacket.PacketSource,
+	recFlows *map[gopacket.Flow]*features.Flow,
+	BWList *map[string]utils.BWInfo,
+	local_ip string,
+) (int, int64) {
 
-	recFlows := make(map[gopacket.Flow]*features.Flow)
-	// Initialize BWList (Seperate from recFlows because key is source address)
-	BWList := make(map[string]utils.BWInfo)
 	//Start
 	iterCount := 0
 	var iterDuration int64 = 0
@@ -80,9 +82,9 @@ func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, 
 
 		//Flow Creation or Add Packet to flow
 		var flow *features.Flow
-		if f, ok := recFlows[netFlow]; ok {
+		if f, ok := (*recFlows)[netFlow]; ok {
 			flow = f
-			recFlows[netFlow].AddPacket(*packet)
+			(*recFlows)[netFlow].AddPacket(*packet)
 		} else {
 			flow = new(features.Flow)
 			flow.Init(
@@ -94,17 +96,17 @@ func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, 
 				tcpdst.String(),
 				currTime,
 			)
-			recFlows[netFlow] = flow
+			(*recFlows)[netFlow] = flow
 		}
 
 		// TODO6: Recheck WL / BL => Remove the WL from the BWList, Forward the BL to NFQueue?
 		key := flow.ClientIP + ":" + flow.ClientPort
 		var isWL bool
-		if info, isExist := BWList[key]; isExist {
+		if info, isExist := (*BWList)[key]; isExist {
 			isWL = info.Bw == "white"
 			if isWL && time.Now().Sub(info.LastCheck) > Config.WLRecheckInterval { //Pass to model
 				info.Bw = "recheck"
-				BWList[key] = info
+				(*BWList)[key] = info
 			}
 		}
 
@@ -133,7 +135,7 @@ func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, 
 			// TODO3-1: Add to BWL __DONE__
 			// TODO4: Process BWL => BL exec command line: iptables, __DONE__
 			if isMalicious {
-				BWList[key] = utils.BWInfo{
+				(*BWList)[key] = utils.BWInfo{
 					Bw:        "black", //true means black list
 					LastCheck: time.Now(),
 				}
@@ -142,7 +144,7 @@ func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, 
 			} else { //Check if the duration is enough to be in WL
 				flowDuration := flow.GetFlowDuration()
 				if flowDuration > Config.WLDurationThreshold.Milliseconds() {
-					BWList[key] = utils.BWInfo{
+					(*BWList)[key] = utils.BWInfo{
 						Bw:        "white", //true means black list
 						LastCheck: time.Now(),
 					}
@@ -155,77 +157,60 @@ func processPackets(packetSource *gopacket.PacketSource, local_ip string) (int, 
 
 	}
 	var featuresList []map[string]interface{}
-	for _, flow := range recFlows {
+	for _, flow := range *recFlows {
 		featuresList = append(featuresList, flow.GetFullFeatures())
 	}
 	// volumePath := "/pv/pv1/"
 	volumePath := "./"
 	utils.WriteMapsToCSV(featuresList, volumePath+"output.csv")
 	// TODO3-2: Save to CSV
-	utils.WriteBWL_toCSV(BWList)
+	utils.WriteBWL_toCSV((*BWList))
 	return iterCount, iterDuration
 }
 
 func main() {
-	//Get Command Line Arguments
-	path := flag.String("path", "./tcpdump/mycapture.pcap", "Path to Pcap File")
-	flag.Parse()
-	print(path, "\n")
-<<<<<<< HEAD
-	//Set timer
 
-	//Profiling
-	cpuUsage := make(chan float64)
-	cpuBefore, _ := cpu.Percent(3*time.Second, false)
-
-	// Memory
-	memUsage := make(chan float64)
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	MemBefore := m.TotalAlloc
-	run_ticker := time.NewTicker(20 * time.Second)
-	go measureCPU(cpuUsage, memUsage, run_ticker) //CPU
-=======
->>>>>>> 143a676d8fdb3120e55e707ddcec9802969fef13
-
-	//Get Local IP
-	// addrs_arr, _ := net.InterfaceAddrs()
-	// var local_ip string
-	// for _, addrs := range addrs_arr {
-	// 	if ipnet, ok := addrs.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-	// 		ipAddr := ipnet.IP.String()
-	// 		if strings.HasPrefix(ipAddr, "192.") {
-	// 			local_ip = ipAddr
-	// 			break
-	// 		}
-	// 	}
-	// }
 	local_ip := "172.16.189.72"
 	fmt.Printf("Running Packet Filtering in %s \n", local_ip)
 
 	// Use Pcap to capture packets
 	// handle, err := pcap.OpenLive(*netInterface, 1600, true, pcap.BlockForever) //ifconfig to see active network interface
-<<<<<<< HEAD
-	handle, err := pcap.OpenOffline(path)
-=======
-	handle, err := pcap.OpenOffline(*path)
->>>>>>> 143a676d8fdb3120e55e707ddcec9802969fef13
+
+	// Read files
+	file_initial := "./mycapture_"
+	files, err := filepath.Glob(file_initial)
+	totIterCount := 0
+	totIterDuration := 0
+	for _, file := range files {
+		fmt.Println(file)
+	}
+	recFlows := make(map[gopacket.Flow]*features.Flow)
+	BWList := make(map[string]utils.BWInfo) // Initialize BWList (Seperate from recFlows because key is source address)
+	for _, file := range files {
+		handle, err := pcap.OpenOffline(file)
+		if err != nil {
+			log.Printf("Error opening pcap file %s: %v\n", file, err)
+			continue
+		}
+		// var filter string = "tcp" //Add more e.g "tcp and port 80"
+		// err = handle.SetBPFFilter(filter)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		defer handle.Close()
+
+		iterCount, iterDuration := processPackets(packetSource, &recFlows, &BWList, local_ip)
+
+		totIterCount += iterCount
+		totIterDuration += int(iterDuration)
+	}
+
 	if err != nil {
 		log.Fatal((err))
 	}
-	defer handle.Close()
-
-	var filter string = "tcp" //Add more e.g "tcp and port 80"
-	err = handle.SetBPFFilter(filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-
-	iterCount, iterDuration := processPackets(packetSource, local_ip)
 
 	//Profiling
-	fmt.Printf("Total Number of Packets %d \n", iterCount)
-	fmt.Printf("Duration of all iterations %d \n", iterDuration)
+	fmt.Printf("Total Number of Packets %d \n", totIterCount)
+	fmt.Printf("Duration of all iterations %d \n", totIterDuration)
 }

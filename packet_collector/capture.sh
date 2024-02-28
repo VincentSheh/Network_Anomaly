114bf9d98f29344adec9b0bf08f16c565975ac71
Sheh Vincent <sheh.v123@gmail.com>
2024-02-28 12:02:44 +0800

    resource usage

--- packet_collector/capture.sh
+++ packet_collector/capture.sh
@@ -66,7 +66,7 @@ while true; do
     # echo "Merged pcap files into $filename"
 
     filename="capture_$(date +%Y%m%d%H%M%S).pcap"
-    timeout "$duration" tcpdump -i enp0s3 "$filter_condition" -w "$filename" &
+    timeout "$duration" tcpdump -i enp0s3 "$filter_condition" -w "$filename"
     wait
     echo "Pcap files created $filename"
 

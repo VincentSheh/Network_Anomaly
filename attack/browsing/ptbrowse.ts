c225ca366740172a117221ab6952bd63b62caadc
Sheh Vincent <sheh.v123@gmail.com>
2024-03-17 18:14:43 +0800

    browsing emulate

---
+++ attack/browsing/ptbrowse.ts
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer');
+// Array of location, then select the location randomly
+function getRandomPlace(placesArray) {
+  const randomIndex = Math.floor(Math.random() * placesArray.length);
+  return placesArray[randomIndex];
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const delay = (ms) => new Promise(res => setTimeout(res, ms));
+
+(async ()=> {
+  // Launch a browser instance
+  try{
+    while (true){
+      const places = ["Taipei 101", "Tamsui", "Ximen", "Taipei Main Station"]
+      let browser = await puppeteer.launch({
+        headless: false,
+        args: [
+          `--no-sandbox`,
+          '--disable-setuid-sandbox',
+        ],
+      });  // Open a new page
+      const page = await browser.newPage();
+      
+      // Go to a webpage that contains a Google Map.
+      // Replace 'your_website_with_map.com' with the actual URL
+      await page.goto('http://localhost:3000/map');    
+      for (let i=0;i<getRandomInt(25); i++){
+        const searchSelector = '.pac-target-input'; // Using one of the provided classes
+        await page.waitForSelector(searchSelector);
+    
+        await page.type(searchSelector, getRandomPlace(places));
+    
+        const goButtonSelector = 'button.px-2.text-white.bg-black.border-l.rounded';
+        await page.waitForSelector(goButtonSelector); // Wait for the button to be ready
+        await page.click(goButtonSelector); // Click the button
+        await delay(getRandomInt(10000))
+        await page.evaluate((selector) => {
+          document.querySelector(selector).value = ''; // Clear the input field
+        }, searchSelector);        
+      }
+      page.close()
+    }
+
+
+  } catch (err) {
+    console.error(err);
+  }
+})();
+

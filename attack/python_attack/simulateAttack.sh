9d32935b2eaf7743bdc3a235a203d0f325cb07e4
Sheh Vincent <sheh.v123@gmail.com>
2024-03-17 10:18:43 +0800

    automate attack

--- attack/python_attack/simulateAttack.sh
+++ attack/python_attack/simulateAttack.sh
@@ -9,6 +9,7 @@ commands=(
 # Loop through each command
 for cmd in "${commands[@]}"; do
   echo "Starting command: $cmd"
+
   # Start the command in the background
   $cmd &
   # Get the PID of the command just run
@@ -16,24 +17,12 @@ for cmd in "${commands[@]}"; do
   # Wait for 180 seconds before attempting to kill the command
   sleep 30
   # Attempt to kill the process
-  kill $cmd_pid 2>/dev/null
+  kill -SIGINT $cmd_pid
 
-  # Wait a moment to let the kill command take effect
-  sleep 2
+  wait $PID
 
-  # Check if the process is still running
-  if kill -0 $cmd_pid 2>/dev/null; then
-    echo "Process $cmd_pid resisted termination, attempting force kill..."
-    kill -9 $cmd_pid
-    sleep 1  # Give some time for the process to be forcefully stopped
-    if kill -0 $cmd_pid 2>/dev/null; then
-      echo "Failed to kill process $cmd_pid"
-    else
-      echo "Process $cmd_pid was successfully killed with force."
-    fi
-  else
-    echo "Process $cmd_pid was successfully killed."
-  fi
+
+
 done
 
 echo "All commands have been executed."

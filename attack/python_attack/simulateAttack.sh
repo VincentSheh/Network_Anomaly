b4896c84ec5640a1a603736deecef9ac9a375ced
Sheh Vincent <sheh.v123@gmail.com>
2024-03-17 11:02:59 +0800

    automate attack

--- attack/python_attack/simulateAttack.sh
+++ attack/python_attack/simulateAttack.sh
@@ -11,15 +11,15 @@ for cmd in "${commands[@]}"; do
   echo "Starting command: $cmd"
 
   # Start the command in the background
-  $cmd &
+  # $cmd &
   # Get the PID of the command just run
-  cmd_pid=$!
+  # cmd_pid=$!
   # Wait for 180 seconds before attempting to kill the command
   sleep 15
   # Attempt to kill the process
-  kill -SIGTERM $PID
+  pkill -f 'python3 ./dos_ge/goldeneye.py http://parkingtracker.com'
 
-  wait $PID
+  # wait $PID
   sleep 10
 
 

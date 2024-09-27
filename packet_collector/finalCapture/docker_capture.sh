752b9f49cb69eb8c0acd8ee319cdeb75b646c6c4
VincentSheh <sheh.v123@gmail.com>
2024-09-27 09:59:09 +0800

    state machine

---
+++ packet_collector/finalCapture/docker_capture.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+#csv Filename
+usage() {
+    echo "Usage: $0 -f filename"
+    echo "  -f    specify the CSV filename"
+    echo "  -h    display this help message"
+    exit 1
+}
+
+csvFolder="output"    
+while getopts hf: flag
+do
+    case "${flag}" in
+        h) usage;;
+        f) csvFolder=${OPTARG};;
+    esac
+done
+echo $csvFolder
+# Function to handle cleanup on exit
+cleanup() {
+    echo "Caught SIGINT signal! Cleaning up..."
+    # Add any cleanup tasks here
+    exit 0
+}
+
+# Trap SIGINT (Ctrl+C)
+trap cleanup SIGINT
+ 
+while true; do #TODO: Change this to a list of duration
+    local_IDS_state=0
+    iter_count=0
+    duration=10
+    # Get all the cali network interface name
+    # grepInterfaces=$(ip link show | grep -o 'cali[[:alnum:]]*')
+    # readarray -t netInterfaces <<< "$grepInterfaces"
+    # Array to store pcap filenames
+    pcap_files=()
+
+        
+    # Loop over each interface and start tcpdump in the background
+    # host_ip="172.16.189.72" #Ingress-nginx
+    # host_ip="172.16.189.71" #Metallb
+    host_ip="192.168.50.30" #Ingress-nginx external IP
+    # Insert the kubernetes IPs here
+    # excluded_ips=("10.96.0.1" "172.16.189.73" "172.16.166.128" "172.16.235.129" "192.168.50.228")
+    # excluded_ips=("10.96.0.1" "172.16.189.73" "172.16.166.128" "172.16.235.129")
+    excluded_ips=("192.168.50.54" "192.168.50.228")
+
+    filter_condition="host $host_ip"
+    for ip in "${excluded_ips[@]}"; do
+        # if [ -z "$filter_condition"]; then
+        #     filter_condition+="not host $ip"
+        # else
+            filter_condition+=" and not host $ip"
+        # fi
+    done    
+    filter_condition="host 192.168.50.12 and (host 192.168.50.211 || host 192.168.50.181)"
+    # filter_condition="host 192.168.50.12 and host 192.168.50.181"
+
+    # for intf in "${netInterfaces[@]}"; do
+    #     filename="${intf}_capture.pcap"
+    #     pcap_files+=("$filename") # Add filename to array
+    #     echo "Starting packet capture on $intf for $duration seconds"
+    #     timeout "$duration" tcpdump -i "$intf" "$filter_condition" -w "$filename" &
+    # done
+
+    # ? Wait for all tcpdump processes to finish
+    # wait
+
+    # # Merge pcap files into one using joincap
+    # filename="capture_$(date +%Y%m%d%H%M%S).pcap"
+    # joincap -w "$filename" "${pcap_files[@]}"
+    # echo "Merged pcap files into $filename"
+    name="$(date +%Y%m%d%H%M%S)"
+    filename="capture_$name.pcap"
+    timeout "$duration" tcpdump -i wlo1 "$filter_condition" -w "$filename"
+    wait
+    echo "Pcap files created $filename"
+
+    # TODO: Obtain the IP of the Ingress Controller and perform pass it as arguments
+    # ? Using CICFlowMeter
+    ./cicflowmeter/convert_pcap_csv.sh "$merged_name" "$csvFolder"
+
+
+    # sudo /home/vs/miniconda3/bin/python upload_csv.py "./cicflowmeter/$csvFolder/merged_${name}_ISCX.csv"
+
+    # #? Update the State Machine
+    # if ["$iter_count" -eq 20]; then
+    #     local_IDS_state=1
+    # fi
+    # #TODO: Get QoE Metric from QoE Analyzer and decide change of state
+done
+
+
+
+
+
+
+# while true; do
+
+#     # Run packet capture for 5 minutes
+#     # Duration for tcpdump to run on each interface
+#     duration=200
+#     # duration=10
+
+#     # Get all the cali network interface name
+#     # grepInterfaces=$(ip link show | grep -o 'cali[[:alnum:]]*')
+#     # readarray -t netInterfaces <<< "$grepInterfaces"
+#     # Array to store pcap filenames
+#     pcap_files=()
+
+        
+#     # Loop over each interface and start tcpdump in the background
+#     # host_ip="172.16.189.72" #Ingress-nginx
+#     # host_ip="172.16.189.71" #Metallb
+#     host_ip="192.168.50.30" #Ingress-nginx external IP
+#     # Insert the kubernetes IPs here
+#     # excluded_ips=("10.96.0.1" "172.16.189.73" "172.16.166.128" "172.16.235.129" "192.168.50.228")
+#     # excluded_ips=("10.96.0.1" "172.16.189.73" "172.16.166.128" "172.16.235.129")
+#     excluded_ips=("192.168.50.112" "192.168.50.228")
+
+#     filter_condition="host $host_ip"
+#     for ip in "${excluded_ips[@]}"; do
+#         # if [ -z "$filter_condition"]; then
+#         #     filter_condition+="not host $ip"
+#         # else
+#             filter_condition+=" and not host $ip"
+#         # fi
+#     done    
+#     # filter_condition="host 192.168.50.12 and (host 192.168.50.211 || host 192.168.50.181)"
+#     filter_condition="host 192.168.50.12 and host 192.168.50.181"
+
+#     # for intf in "${netInterfaces[@]}"; do
+#     #     filename="${intf}_capture.pcap"
+#     #     pcap_files+=("$filename") # Add filename to array
+#     #     echo "Starting packet capture on $intf for $duration seconds"
+#     #     timeout "$duration" tcpdump -i "$intf" "$filter_condition" -w "$filename" &
+#     # done
+
+#     # # Wait for all tcpdump processes to finish
+#     # wait
+
+#     # # Merge pcap files into one using joincap
+#     # filename="capture_$(date +%Y%m%d%H%M%S).pcap"
+#     # joincap -w "$filename" "${pcap_files[@]}"
+#     # echo "Merged pcap files into $filename"
+#     name="$(date +%Y%m%d%H%M%S)"
+#     filename="capture_$name.pcap"
+#     timeout "$duration" tcpdump -i wlo1 "$filter_condition" -w "$filename"
+#     wait
+#     echo "Pcap files created $filename"
+
+
+
+#     # Delete the oldest pcap file
+#     # Find all pcap files, sort them, and delete all but the two most recent
+#     ls -1tr capture_*.pcap | head -n -1 | xargs -d '\n' rm -f --
+#     #TODOOO: Merge the 2 pcap file into one and go run . on the merged file
+#     filelist=()
+#     for file in capture_*; do
+#         if [ -e "$file" ]; then # Check if the file exists
+#             filelist+=("$file")
+#             echo "$file"
+#         fi
+#     done
+#     echo "Reading ${filelist[@]} \n"
+#     merged_name="merged_${name}.pcap"
+#     joincap -w "$merged_name" "${filelist[@]}"
+
+
+#     #Concatenate with cumalated pcap file
+#     # $
+#     # mv temp_cumulated.pcap cumulated.pcap
+
+#     # Run Go code to process the two latest pcap files
+#     # TODO: Obtain the IP of the Ingress Controller and perform pass it as arguments
+#     # sudo /usr/local/go/bin/go run .
+#     # ? Using Go to convert pcap to csv
+#     # sudo go run . --filename=$csvFilename
+#     # ./packet_collector --filename test.csv
+#     # ./packet_collector
+#     # ? Using CICFlowMeter
+#     ./cicflowmeter/convert_pcap_csv.sh "$merged_name" "$csvFolder"
+#     # Repeat indefinitely
+#     # sleep 5
+#     # ! Merge the merged_*.pcap files
+#     mergefilelist=()
+#     for file in merged_*; do
+#         if [ -e "$file" ]; then # Check if the file exists
+#             mergefilelist+=("$file")
+#             echo "$file"
+#         fi
+#     done  
+#     joincap -w "cumulated.pcap" "${mergefilelist[@]}"  
+#     ls -1tr merged_*.pcap | xargs -d '\n' rm -f --    
+
+#     # TODO: Run python upload_csv -f {$csv_file}
+#     # sudo /home/vs/miniconda3/bin/python upload_csv.py "./cicflowmeter/$csvFolder/merged_${name}_ISCX.csv"
+# done
---
+++ packet_collector/finalCapture/upload_csv.py
@@ -0,0 +1,163 @@
+import requests
+import argparse
+import pandas as pd
+import pickle
+from io import StringIO
+import numpy as np
+import os
+
+def record_csv(result_df, file_path):
+    unlabeled_df = pd.read_csv(file_path)
+    unlabeled_df.to_csv("x.csv", index=False)
+    result_df.to_csv("result.csv", index=False)
+    unlabeled_df["Label"] = result_df["Label"]
+    
+    # Get Available Filename
+    filenumber = 0
+    folder = "./cicflowmeter/s0_lm_training_data/"
+    filename = "train"
+    filepath = os.path.join(folder,f"{filename}_{filenumber}.csv")
+    while os.path.exists(filepath):
+        filenumber+=1
+        filepath = os.path.join(folder, f"{filename}_{filenumber}.csv")  
+    # Write to CSV
+    unlabeled_df.to_csv(filepath, index=False)
+    print("record_csv() Saved File to", filepath)  
+    
+    
+def retrain_lm(training_folder, url):
+    # Send the retraining request to LM
+    merge_csv(training_folder)
+    files = {'file': open(training_folder+"final_merged.csv", 'rb')}
+    print(url, files['file'])
+    response = requests.post(url, files=files)
+    if response.status_code == 200:
+        print("\033[32mLocal Model Started Training\033[0m")
+    else:
+        print(f"\033[31mFailed to start training: {response.status_code} {response.text}\033[0m")
+    # Flush Training Folder file
+    
+    
+def send_csv(url, file_path, record_flag, offload_url = ""): 
+    data = {'offload_url': offload_url}
+    files = {'file': open(file_path, 'rb')}
+    print(files["file"], data)
+    response = requests.post(url, files=files, data=data)
+    if response.status_code == 200:
+        # Convert JSON response back into DataFrame
+        json_data = response.json()
+        df = pd.DataFrame(json_data, columns=["origin_ip", "Label"])
+        if record_flag:
+            print("Recording Data")
+            record_csv(df, file_path)
+        return df
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+        return None
+
+def merge_csv(file_path):
+    # Get the directory of the provided file path
+    directory = os.path.dirname(file_path)
+
+    # List all files in the directory
+    path_list = os.listdir(directory)
+
+    # Create an empty DataFrame to hold the merged content
+    full_df = pd.DataFrame()
+
+    # Loop through the list of files to read, merge, and delete them
+    for file in path_list:
+        full_file_path = os.path.join(directory, file)
+        try:
+            # Attempt to read the current file into a DataFrame
+            curr_file_df = pd.read_csv(full_file_path)
+            if not curr_file_df.empty:
+                # Concatenate the current DataFrame with the full DataFrame
+                full_df = pd.concat([curr_file_df, full_df], axis=0, ignore_index=True)
+            # Delete the file after reading
+            os.remove(full_file_path)
+            print(f"Deleted file: {full_file_path}")
+        except Exception as e:
+            print(f"Error reading or deleting file {full_file_path}: {e}")
+
+    # Save the merged DataFrame to a new CSV file
+    final_merged_path = os.path.join(directory, "final_merged.csv")
+    full_df.to_csv(final_merged_path, index=False)
+    print(f"Merged all CSV files into {final_merged_path}")
+    
+    print(f"Merged CSV Files {path_list} in {directory}")
+    
+def get_state_parameters(state, lm_url, gm_url):
+    time_ellapsed = 0
+    if state == 0: #Send Detection Task directly to the Selected Model and Record the Datas for training
+        decision = "detect"
+        record_flag = True
+        time_ellapsed += 1 #TODO: Record in terminal script
+        url = gm_url #? Or other_lm_url
+        offload_url = ""
+    if state == 1: #Send Detection Task to Local Model
+        decision = "detect"
+        record_flag = False
+        url = lm_url
+        offload_url = ""
+    if state == 2: #Send Detection Task to Local Model which will forward it to selected LM
+        decision = "offload"
+        record_flag = False
+        url = lm_url #? Or other_lm_url
+        offload_url = gm_url + decision
+    if state == 3: #Send Recorded Training Data to GM then Retrain and #TODO: Perform Training of LM
+        decision = "retrain"
+        record_flag = True
+        url = gm_url
+        offload_url = ""
+    url = url + decision
+        
+    return record_flag, url, offload_url
+
+def main():
+    parser = argparse.ArgumentParser(description='Upload File to CSV.')
+    parser.add_argument('-f','--file_path', type=str, help='Path to the CSV file to send.')
+    parser.add_argument('-s','--curr_state', type=int, help='Current State of the Local IDS: 0 1 2 3')
+    parser.add_argument('-t','--transition_to_on', action='store_true', help='Whether to transition from s0 to s1')
+    args = parser.parse_args()
+    #TODO: Query LMM for offloading URL
+    lm_url = "http://192.168.50.167:3001/"
+    gm_url = "http://192.168.50.167:5050/"
+
+    
+    record_flag, url, offload_url = get_state_parameters(args.curr_state, lm_url, gm_url)
+    if args.transition_to_on == True: # S0 --> S1 Transition
+        print("Retraining LM")
+        retrain_lm("./cicflowmeter/s0_lm_training_data/", lm_url + "retrain")
+    if args.curr_state == 3: #!S3: Doesn't perform detection?
+        print("Training GM..")
+        send_csv(url, args.file_path, record_flag, offload_url)     
+        print("\033[32mFinish Training GM\033[0m")    
+    try:
+        labeled_df = send_csv(url, args.file_path, record_flag, offload_url) # TODO: if offload, incorporate the url to post request
+        if labeled_df is not None and not labeled_df.empty:
+            labeled_df.to_csv("ip_labels.csv", index=False)
+            malicious_ip = labeled_df.loc[labeled_df["Label"] == 1, "origin_ip"]
+            print(np.unique(malicious_ip))
+            
+            if len(malicious_ip) > 0:
+                for ip in np.unique(malicious_ip):
+                    print(ip)
+                    # print(f"Blocked {ip}")
+                    # os.system(f"sudo iptables -A INPUT -s {ip} -j DROP")
+                    # os.system("sudo iptables -L INPUT --line-numbers | wc -l")
+            else:
+                print("\033[32mNo Malicious Traffic Detected\033[0m")
+            merge_csv(args.file_path)
+        else:
+            print("No data returned from the server or the DataFrame is empty.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    # Merge All CSV File in the directory
+    # merge_csv(args.file_path)
+    return 0
+
+if __name__ == '__main__':
+    main()
+    #sudo /home/vs/miniconda3/bin/python upload_csv.py /home/vs/Desktop/Network_Anomaly/packet_collector/cicflowmeter/attack/merged_20240809091837_ISCX.csv
+    
